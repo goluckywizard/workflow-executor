c45f8ebacb0a42b9d600825faed12a87438bbdad
goluckywizard <73885997+goluckywizard@users.noreply.github.com>
2020-12-11 19:54:38 +0700

    Update workflow.cpp

--- workflow.cpp
+++ workflow.cpp
@@ -4,75 +4,136 @@ Reader::Reader(const std::string &filename) {
     input_txt = filename;
 }
 
-void Reader::execute(std::vector<std::string> &Text) {
-    std::ifstream input;
-    input.open(input_txt);
-    std::string str;
-    while (std::getline(input, str))
-    {
-        Text.push_back(str);
+void Reader::execute(std::optional<std::vector<std::string>> &Text) {
+    try {
+        if (Text)
+        {
+            throw "File is already open";
+        }
+        std::ifstream input;
+        input.open(input_txt);
+        if (!input.is_open()) {
+            throw "Input file isn't exist's";
+        }
+        std::vector<std::string> vec;
+        Text.emplace(vec);
+        std::string str;
+        while (std::getline(input, str))
+        {
+            Text->push_back(str);
+        }
+        input.close();
     }
-    input.close();
+    catch (const char* exception) {
+        std::cout << exception;
+    }
 }
 
 Writer::Writer(const std::string &filename){
     output_txt = filename;
 }
-void Writer::execute(std::vector<std::string> &Text) {
-    std::ofstream output;
-    output.open(output_txt);
-    for (const auto& iter : Text)
-    {
-        output << iter << std::endl;
+void Writer::execute(std::optional<std::vector<std::string>> &Text) {
+    try {
+        if (!Text) {
+            throw "File is already closed or hasn't ever opened";
+        }
+        std::ofstream output;
+        output.open(output_txt);
+        if (!output.is_open()) {
+            throw "Output file isn't exist's";
+        }
+        for (const auto& iter : *Text)
+        {
+            output << iter << std::endl;
+        }
+        output.close();
+        Text.reset();
     }
-    output.close();
+    catch (const char *exception) {
+        std::cout << exception;
+    }
 }
 
 Greper::Greper(const std::string &word){
     fword = word;
 }
-void Greper::execute(std::vector<std::string> &Text) {
-    auto iter = Text.begin();
-    while (iter != Text.end()) {
-    std::regex expression("\\b"+fword+"\\b");
-    std::sregex_token_iterator first(iter->begin(), iter->end(), expression);
-    std::sregex_token_iterator last;
-    if (std::distance(first, last) == 0) {
-        iter = Text.erase(iter);
+void Greper::execute(std::optional<std::vector<std::string>> &Text) {
+    try {
+        if (!Text) {
+            throw "File is already closed or hasn't ever opened";
+        }
+        auto iter = Text->begin();
+        while (iter != Text->end()) {
+            std::regex expression("\\b"+fword+"\\b");
+            std::sregex_token_iterator first(iter->begin(), iter->end(), expression);
+            std::sregex_token_iterator last;
+            if (std::distance(first, last) == 0) {
+                iter = Text->erase(iter);
+            }
+            else
+                ++iter;
+        }
     }
-    else
-        ++iter;
+    catch (const char *exception) {
+        std::cout << exception;
     }
 }
 
-void Sorter::execute(std::vector<std::string> &Text) {
-    std::sort(Text.begin(), Text.end());
+void Sorter::execute(std::optional<std::vector<std::string>> &Text) {
+    try {
+        if (!Text) {
+            throw "File is already closed or hasn't ever opened";
+        }
+        std::sort(Text->begin(), Text->end());
+    }
+    catch (const char *exception) {
+        std::cout << exception;
+    }
 }
 
 Replacer::Replacer(const std::string &word1, const std::string &word2){
     old_word = word1;
     new_word = word2;
 }
-void Replacer::execute(std::vector<std::string> &Text) {
-    const std::regex space_extractor("\\b"+old_word+"\\b");
-    auto iter = Text.begin();
-    while (iter != Text.end()) {
-        *iter = std::regex_replace(*iter, space_extractor, new_word);
-        ++iter;
+void Replacer::execute(std::optional<std::vector<std::string>> &Text) {
+    try {
+        if (!Text) {
+            throw "File is already closed or hasn't ever opened";
+        }
+        const std::regex space_extractor("\\b"+old_word+"\\b");
+        auto iter = Text->begin();
+        while (iter != Text->end()) {
+            *iter = std::regex_replace(*iter, space_extractor, new_word);
+            ++iter;
+        }
+    }
+    catch (const char *exception) {
+        std::cout << exception;
     }
 }
 
 Dumper::Dumper(const std::string &file) {
     filename = file;
 }
-void Dumper::execute(std::vector<std::string> &Text) {
-    std::ofstream out;
-    out.open(filename);
-    for (const auto& iter : Text)
-    {
-        out << iter << std::endl;
+void Dumper::execute(std::optional<std::vector<std::string>> &Text) {
+    try {
+        if (!Text) {
+            throw "File is already closed or hasn't ever opened";
+        }
+        std::ofstream out;
+        out.open(filename);
+        if (!out.is_open()) {
+            throw "Output file isn't exist's";
+        }
+        for (const auto& iter : *Text)
+        {
+            out << iter << std::endl;
+        }
+        out.close();
     }
-    out.close();
+    catch (const char *exception) {
+        std::cout << exception;
+    }
 }
 
 
