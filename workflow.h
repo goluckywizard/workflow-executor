8c373e0991b1376421881fb54354c0d7bf209d18
goluckywizard <73885997+goluckywizard@users.noreply.github.com>
2020-12-13 18:00:36 +0700

    Update workflow.h

--- workflow.h
+++ workflow.h
@@ -8,36 +8,98 @@
 #include <optional>
 #include <memory>
 #include <map>
+class Option_text {
+public:
+    bool isOpen = false;
+    bool hasAltInput = false;
+    bool hasAltOutput = false;
+    bool wasAltInput = false;
+    bool wasAltOutput = false;
+    std::string altInputName;
+    std::string altOutputName;
+    void set_input(std::string &filename) {
+        hasAltInput = true;
+        altInputName = filename;
+    }
+    void set_output(std::string &filename) {
+        hasAltOutput = true;
+        altOutputName = filename;
+    }
+    void alt_read() {
+        try {
+            std::ifstream input;
+            input.open(altInputName);
+            if (!input.is_open()) {
+                throw "Alternative input file isn't exist's";
+            }
+            std::vector<std::string> vec;
+            //Text.emplace(vec);
+            std::string str;
+            while (std::getline(input, str))
+            {
+                text.push_back(str);
+            }
+            input.close();
+            isOpen = true;
+            wasAltInput = true;
+        }
+        catch (const char *exception) {
+            std::cout << exception;
+        }
+    }
+    void alt_write() {
+        try {
+            std::ofstream output;
+            output.open(altOutputName);
+            if (!output.is_open()) {
+                throw "Alternative output file isn't exist's";
+            }
+            for (const auto& iter : text)
+            {
+                output << iter << std::endl;
+            }
+            output.close();
+            text.clear();
+            isOpen = false;
+            wasAltOutput = true;
+        }
+        catch (const char *exception) {
+            std::cout << exception;
+        }
+    }
+    std::vector<std::string> text;
+};
+
 class Executor {
 public:
-    virtual void execute(std::optional<std::vector<std::string>> &Text){}
+    virtual void execute(Option_text &Text){}
 };
 
 class Reader : public Executor {
     std::string input_txt;
 public:
     explicit Reader(const std::string &filename);
-    void execute(std::optional<std::vector<std::string>> &Text) override;
+    void execute(Option_text &Text) override;
 };
 
 class Writer : public Executor {
     std::string output_txt;
 public:
     explicit Writer(const std::string &filename);
-    void execute(std::optional<std::vector<std::string>> &Text) override;
+    void execute(Option_text &Text) override;
 };
 
 class Greper : public Executor {
     std::string fword;
 public:
     explicit Greper(const std::string &word);
-    void execute(std::optional<std::vector<std::string>> &Text) override ;
+    void execute(Option_text &Text) override ;
 };
 
 class Sorter : public Executor {
 public:
     Sorter ()= default;
-    void execute(std::optional<std::vector<std::string>> &Text) override;
+    void execute(Option_text &Text) override;
 };
 
 class Replacer : public Executor {
@@ -45,23 +107,31 @@ class Replacer : public Executor {
     std::string new_word;
 public:
     Replacer(const std::string &word1, const std::string &word2);
-    void execute(std::optional<std::vector<std::string>> &Text) override;
+    void execute(Option_text &Text) override;
 };
 
 class Dumper : public Executor {
     std::string filename;
 public:
     explicit Dumper(const std::string &file);
-    void execute(std::optional<std::vector<std::string>> &Text) override;
+    void execute(Option_text &Text) override;
 };
 
-std::unique_ptr <Executor> create_block(char type, std::string &par1, std::string par2 = nullptr);
+
+std::shared_ptr <Executor> create_block(char type, std::string &par1, std::string par2 = nullptr);
 
 class Workflow_Parser {
 private:
-    std::map<int, std::unique_ptr<Executor>> blocks;
+    std::map<int, std::shared_ptr<Executor>> blocks;
+    std::list<std::shared_ptr<Executor>> block_list;
     std::ifstream input;
-    void listblocks_parser() {
+    //char *tag1 = nullptr;
+    bool hasAlternaiveInput = false;
+    char *alt_i;
+    bool hasAlternaiveOutput = false;
+    char *alt_o;
+
+    void blocks_parser() {
         std::string new_str;
         while (std::getline(input, new_str)) {
             if (new_str == "desc")
@@ -126,7 +196,7 @@ private:
                 if (!(type && block_number && par1)) {
                     throw "Not enough arguments";
                 }
-                std::unique_ptr<Executor> temp_pointer (create_block(*type, *par1, *par2));
+                std::shared_ptr<Executor> temp_pointer (create_block(*type, *par1, *par2));
                 if (blocks[*block_number])
                 {
                     throw "Blocks can't be overloaded";
@@ -141,10 +211,7 @@ private:
                 std::cout << std::endl;
             }
         }
-    }
-    void do_execute() {
-        std::optional<std::vector<std::string>> Text;
-        std::string new_str;
+        //std::string new_str;
         while (std::getline(input, new_str)) {
             static const std::regex to_extractor("[\\d]+");
             auto numbers_begin = std::sregex_iterator(new_str.begin(), new_str.end(), to_extractor);
@@ -157,15 +224,64 @@ private:
                 }
                 else
                 {
-                    blocks[id]->execute(Text);
+                    //blocks[id]->execute(Text);
+                    block_list.push_back(blocks[id]);
                 }
             }
         }
     }
+    void do_execute() {
+
+        Option_text text;
+        if (hasAlternaiveInput) {
+            text.hasAltInput = true;
+            text.altInputName = alt_i;
+        }
+        if (hasAlternaiveOutput) {
+            text.hasAltOutput = true;
+            text.altOutputName = alt_o;
+        }
+
+        for (auto &it : block_list) {
+            it->execute(text);
+        }
+        if (text.isOpen)
+        {
+            if (text.hasAltOutput && !text.wasAltOutput) {
+                text.alt_write();
+            }
+            else {
+                std::cout << "Text aren't closed!";
+            }
+        }
+        //auto iter = block_list.begin();
+        //if (*iter )
+    }
 public:
-    explicit Workflow_Parser(char *in) {
-        input.open(in);
-        listblocks_parser();
+    Workflow_Parser(char *workflow[], int argc) {
+        if (argc > 2) {
+            /*if (strncmp(workflow[2], "-i", 2) == 0){
+                hasAlternaiveInput = true;
+                alt_i = workflow[3];
+            }*/
+            if (strncmp(workflow[2], "-o", 2) == 0){
+                hasAlternaiveOutput = true;
+                alt_o = workflow[3];
+            }
+        }
+        if (argc > 4) {
+            if (strncmp(workflow[4], "-i", 2) == 0){
+                hasAlternaiveInput = true;
+                alt_i = workflow[5];
+            }
+            if (strncmp(workflow[4], "-o", 2) == 0){
+                hasAlternaiveOutput = true;
+                alt_o = workflow[5];
+            }
+        }
+
+        input.open(workflow[1]);
+        blocks_parser();
         do_execute();
     }
 };
