7f615b2d13bde2f1a653e49e1a74611058da5c61
goluckywizard <73885997+goluckywizard@users.noreply.github.com>
2020-12-16 16:54:40 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include "workflow.h"
 
 int main(int argc, char* argv[]) {
-    Workflow_Parser parse(argv, argc);
+    try {
+        Workflow_Parser parse(argv, argc);
+    }
+    catch (WorkflowException exception) {
+        std::cout << "line " << exception.Line_number <<": " << exception.exception;
+    }
+    catch (...) {
+        std::cout << "Unknown error";
+    }
     return 0;
 }
